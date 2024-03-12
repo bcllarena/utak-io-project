96f20ec75b2f469c6a175b50bb4b259240ad8baa
Ben L <bcllarena@gmail.com>
2024-03-12 13:44:39 +0800

    Fix item list error

--- src/components-custom/ItemList.tsx
+++ src/components-custom/ItemList.tsx
@@ -226,52 +226,56 @@ function ItemList() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {itemList?.map((item) => (
-            <TableRow key={item.itemId}>
-              <TableCell className="font-medium">
-                {capitalizePhrase(item.itemName)}
-              </TableCell>
-              <TableCell>{capitalizePhrase(item.itemCategory)}</TableCell>
-              <TableCell className="text-right">{item.itemPrice}</TableCell>
-              <TableCell className="text-right">{item.itemCost}</TableCell>
-              <TableCell className="text-right">{item.itemStock}</TableCell>
-              <TableCell>
-                {item.itemAddOnsSize
-                  ? capitalizePhrase(item.itemAddOns) +
-                    "(" +
-                    capitalizePhrase(item.itemAddOnsSize) +
-                    ")"
-                  : ""}
-              </TableCell>
-              <TableCell>
-                <button
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9"
-                  data-state="closed"
-                  onClick={() => openDeleteDialog(item.itemId, item.itemName)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-4 w-4"
+          {itemList?.map((item) =>
+            item.itemId ? (
+              <TableRow key={item.itemId}>
+                <TableCell className="font-medium">
+                  {capitalizePhrase(item.itemName)}
+                </TableCell>
+                <TableCell>{capitalizePhrase(item.itemCategory)}</TableCell>
+                <TableCell className="text-right">{item.itemPrice}</TableCell>
+                <TableCell className="text-right">{item.itemCost}</TableCell>
+                <TableCell className="text-right">{item.itemStock}</TableCell>
+                <TableCell>
+                  {item.itemAddOnsSize
+                    ? capitalizePhrase(item.itemAddOns) +
+                      "(" +
+                      capitalizePhrase(item.itemAddOnsSize) +
+                      ")"
+                    : ""}
+                </TableCell>
+                <TableCell>
+                  <button
+                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9"
+                    data-state="closed"
+                    onClick={() => openDeleteDialog(item.itemId, item.itemName)}
                   >
-                    <path d="M3 6h18"></path>
-                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-                    <line x1="10" x2="10" y1="11" y2="17"></line>
-                    <line x1="14" x2="14" y1="11" y2="17"></line>
-                  </svg>
-                  <span className="sr-only">Move to trash</span>
-                </button>
-              </TableCell>
-            </TableRow>
-          ))}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="h-4 w-4"
+                    >
+                      <path d="M3 6h18"></path>
+                      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+                      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                      <line x1="10" x2="10" y1="11" y2="17"></line>
+                      <line x1="14" x2="14" y1="11" y2="17"></line>
+                    </svg>
+                    <span className="sr-only">Move to trash</span>
+                  </button>
+                </TableCell>
+              </TableRow>
+            ) : (
+              ""
+            )
+          )}
         </TableBody>
       </Table>
 
